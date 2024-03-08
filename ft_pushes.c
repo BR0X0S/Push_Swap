23155bebe45b3799714722db809251c02ab26274
oumondad <oumondad@e1r9p10.1337.ma>
2024-03-08 15:58:09 +0100

    mini done

--- ft_pushes.c
+++ ft_pushes.c
@@ -1,38 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   ft_pushes.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: oumondad <oumondad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/02 16:57:25 by oumondad          #+#    #+#             */
-/*   Updated: 2024/03/07 14:32:02 by oumondad         ###   ########.fr       */
+/*   Created: 2024/03/07 14:28:07 by oumondad          #+#    #+#             */
+/*   Updated: 2024/03/08 15:57:42 by oumondad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-void	print_stack(t_list *stack_a)
-{
-	while (stack_a)
-	{
-		printf("%d\n", (stack_a)->content);
-		stack_a = (stack_a)->next;
-	}
-}
-
-int	main(int ac, char **av)
-{
-	t_var	data;
-	t_list	*stack_a;
-	t_list	*stack_b;
-
-	if (ac < 2)
-		ft_error("error");
-	stack_a = NULL;
-	stack_b = NULL;
-	data = ft_khchi_fchi(av);
-	array_to_stack(&stack_a, data);
-	print_stack(stack_a);
-	return (0);
-}
